96d104520053080f5b077fe05fb6532db7218aa0
Jacinth-Lu <lujiaxin@sjtu.edu.cn>
2020-03-17 01:55:54 +0800

    Add builtin functions to IRRoot!

--- src/Compiler/IR/IRRoot.java
+++ src/Compiler/IR/IRRoot.java
@@ -15,8 +15,57 @@ public class IRRoot
     private List<StaticStr> staticStrList = new ArrayList<>();
     private List<StaticData> staticDataList = new ArrayList<>();
 
+    public Function builtinPrint = new Function("print", "__builtin_print");
+    public Function builtinPrintln = new Function("println", "__builtin_println");
+    public Function builtinPrintInt = new Function("printInt", "__builtin_printInt");
+    public Function builtinPrintlnInt = new Function("printlnInt", "__builtin_printlnInt");
+    public Function builtinGetString = new Function("getString", "__builtin_getString");
+    public Function builtinGetInt = new Function("getInt", "__builtin_getInt");
+    public Function builtinToString = new Function("toString", "__builtin_toString");
 
-    //TODO: START WITH BUILTIN FUNCTIONS;
+    public Function builtinStringLength = new Function("string.length", "__builtin_string_length");
+    public Function builtinStringSubstring = new Function("string.substring", "__builtin_string_substring");
+    public Function builtinStringParseInt = new Function("string.parseInt", "__builtin_string_parseInt");
+    public Function builtinStringOrd = new Function("string.ord", "__builtin_string_ord");
+    public Function builtinArraySize = new Function("array.size", "__builtin_array_size");
+
+    public Function builtinStringAdd = new Function("string.add", "__builtin_string_add");
+    public Function builtinStringLT = new Function("string.lt", "__builtin_string_lt");
+    public Function builtinStringLEQ = new Function("string.leq", "__builtin_string_leq");
+    public Function builtinStringEQ = new Function("string.eq", "__builtin_string_eq");
+    public Function builtinStringNEQ = new Function("string.neq", "__builtin_string_neq");
+    public Function builtinStringREQ = new Function("string.req", "__builtin_string_req");
+    public Function builtinStringRT = new Function("string.rt", "__builtin_string_rt");
+
+    public IRRoot()
+    {
+        addBuiltinFunction(builtinPrint);
+        addBuiltinFunction(builtinPrintln);
+        addBuiltinFunction(builtinPrintInt);
+        addBuiltinFunction(builtinPrintlnInt);
+        addBuiltinFunction(builtinGetString);
+        addBuiltinFunction(builtinGetInt);
+        addBuiltinFunction(builtinToString);
+        addBuiltinFunction(builtinStringLength);
+        addBuiltinFunction(builtinStringSubstring);
+        addBuiltinFunction(builtinStringParseInt);
+        addBuiltinFunction(builtinStringOrd);
+        addBuiltinFunction(builtinArraySize);
+        addBuiltinFunction(builtinStringAdd);
+        addBuiltinFunction(builtinStringLT);
+        addBuiltinFunction(builtinStringLEQ);
+        addBuiltinFunction(builtinStringEQ);
+        addBuiltinFunction(builtinStringNEQ);
+        addBuiltinFunction(builtinStringREQ);
+        addBuiltinFunction(builtinStringRT);
+    }
+
+    //DONE: START WITH BUILTIN FUNCTIONS;
+
+    public void addBuiltinFunction(Function function)
+    {
+        builtinFunctions.put(function.getName(), function);
+    }
 
     public void addFunction(Function function)
     {

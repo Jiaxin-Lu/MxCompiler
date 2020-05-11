d7a52078a72e6ef8daaa7ff9197677f980fc643c
Jacinth-Lu <lujiaxin@sjtu.edu.cn>
2020-05-12 02:46:55 +0800

    add libs

--- build.bash
+++ build.bash
@@ -4,4 +4,4 @@
 set -e
 cd "$(dirname "$0")"
 mkdir -p bin
-find ./src -name *.java | javac -d bin -classpath "/ulib/java/antlr-4.8-complete.jar" @/dev/stdin
+find ./src -name *.java | javac -d bin -classpath "/ulib/java/antlr-4.8-complete.jar:./lib/commons-text-1.6.jar:./lib/commons-lang3-3.8.1.jar" @/dev/stdin
--- optim.bash
+++ optim.bash
@@ -5,6 +5,6 @@
 
 set -e
 cd "$(dirname "$0")"
-export CCHK="java -classpath /ulib/java/antlr-4.8-complete.jar:./bin Compiler.Main"
+export CCHK="java -classpath /ulib/java/antlr-4.8-complete.jar:./lib/commons-text-1.6.jar:./lib/commons-lang3-3.8.1.jar:./bin Compiler.Main"
 cat > code.mx
 $CCHK
